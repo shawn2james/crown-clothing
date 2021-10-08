2603de9241a6c3355fcfd29c299ee8348c827de7
shawn2james <shawwn2james@gmail.com>
2021-10-08 19:42:49 +0530

    implemeted firebase utils, included ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { Route, Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,9 +19,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // store current user details in currentUser state (when signed in)
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+
+        console.log(this.state);
+      } else {
+        // set currentUser to null if userAuth is null (when signed out)
+        this.setState({ currentUser: userAuth });
+      }
+    })   
   }
 
   componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const config = {
   apiKey: "AIzaSyCemSz32ueTPC6yfH8w_Wn7lfnqYuuxUZ8",
@@ -9,8 +9,7 @@ const config = {
   storageBucket: "crown-db-3be50.appspot.com",
   messagingSenderId: "771528830074",
   appId: "1:771528830074:web:a83bdbda0b26c422721fc3",
-  measurementId: "G-QE0439KC0T"
-
+  measurementId: "G-QE0439KC0T",
 };
 
 firebase.initializeApp(config);
@@ -19,7 +18,38 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // if user is not signed in 
+  if (!userAuth) return;
+
+  // get QueryReference object
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // get QuerySnapshot object
+  const snapshot = await userRef.get();
+
+  // if user doesn't exist in database
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth
+    ;
+    const createdAt = new Date();
+
+    // add user to database
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user...", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export default firebase;
